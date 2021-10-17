60d5e6c07021affab7a427912edb62e43811e437
Jaco Destanovic <jaco.destanovic@gmail.com>
2021-10-17 20:08:31 +0200

    Function to GetData.py added. DAX symbols as txt-file added. Configuration file updated with DAX list and S&P lis

--- Configuration_StOb.py
+++ Configuration_StOb.py
@@ -1,1 +1,16 @@
-# cofig file
+# config file
+
+import pandas as pd
+
+''' Comparing values at given date'''
+first_comp = 2 # compare last two available end of day data
+second_comp = 7 # compare last available end of data day and seven days ago
+third_comp = 30 # compare last available end of day data
+
+with open("S_P_Symbols.txt") as file:
+    s_p_symbs = [line.strip() for line in file]
+
+with open("DAX_Symbols.txt") as f:
+    dax_symbs = [line.strip() for line in f]
+
+exchanges = ["NASDAQ", "XETR"]
--- GetData.py
+++ GetData.py
@@ -6,10 +6,16 @@ import numpy as np
 import pandas as pd
 from Configuration_StOb import *
 
-
 tdf = TvDatafeed(auto_login = False, chromedriver_path = '/usr/bin/chromedriver')
 
-hist = tdf.get_hist(symbol = 'MSFT', exchange = 'NASDAQ', interval = Interval.in_daily, n_bars = 1000)
-hist_close = hist.close
+def get_data(symbols):
+    for i in range(len(symbols)):
+        hist = tdf.get_hist(symbol = symbols[i], exchange = "XETR", interval = Interval.in_daily, n_bars = 1000)
+        hist_reverse = hist[::-1]
+        print(hist_reverse.head())
+        print(type(hist))
 
-print(hist.close)
+#hist = tdf.get_hist(symbol = "ADS", exchange = "XETR", interval = Interval.in_daily, n_bars = 1000)
+get_data(dax_symbs)
+#print(hist.head())
+#print(hist.close)
