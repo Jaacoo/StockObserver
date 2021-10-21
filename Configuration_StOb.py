a9563912849ae74cef4fac035e5eb6d939e93a6c
Jaco Destanovic <jaco.destanovic@gmail.com>
2021-10-21 16:17:20 +0200

    Updates GetData.py to return dicitonary. Added rolling window to Configuration_StOb.py

--- Configuration_StOb.py
+++ Configuration_StOb.py
@@ -7,13 +7,13 @@ first_comp = 2 # compare last two available end of day data
 second_comp = 7 # compare last available end of data day and seven days ago
 third_comp = 30 # compare last available end of day data
 
-with open("NYSE_Symbols.txt") as file:
+with open("NYSE_Symbols_test.txt") as file:
     nyse_symbs = [line.strip() for line in file]
 
-with open("NASDAQ_Symbols.txt") as f:
+with open("NASDAQ_Symbols_test.txt") as f:
     nasdaq_symbs = [line.strip() for line in f]
 
-with open("DAX_Symbols.txt") as f:
+with open("DAX_Symbols_test.txt") as f:
     dax_symbs = [line.strip() for line in f]
 
 exchanges_list = ["XETR", "NYSE", "NASDAQ"]
--- GetData.py
+++ GetData.py
@@ -1,45 +1,26 @@
 
-
 from tvDatafeed import TvDatafeed, Interval
 import datetime
-import numpy as np
 import pandas as pd
 from Configuration_StOb import *
 
-tdf = TvDatafeed(auto_login = False, chromedriver_path = '/usr/bin/chromedriver')
 
 def get_data(exchange_symbols, interval, bars):
-    data_frame = []
+    tdf = TvDatafeed(auto_login = False, chromedriver_path = '/usr/bin/chromedriver')
+    data_frame_dict = {}
+    data_frame_list = []
     for key in exchange_symbols.keys():
         print(key)
         for i in range(len(exchange_symbols[key])):
+            print(exchange_symbols[key][i])
             hist = tdf.get_hist(symbol = exchange_symbols[key][i], exchange = key, interval = interval, n_bars = bars)
-            hist_reverse = hist[::-1]
-            hist_close = hist_reverse[['close']]
+            hist_close = hist[['close']]
             hist_renamed = hist_close.rename(columns = {'close' : exchange_symbols[key][i]})
-            #hist_final = pd.concat()
-            data_frame.append(hist_renamed)
-            #print(data_frame.shape)
-            #print(hist_renamed.head(10))
-            #print(type(hist))
-    hist_final = pd.concat(data_frame, axis = 1)
-    #print(hist_final.head())
+            data_frame_list.append(hist_renamed)
+            if(i==len(exchange_symbols[key])-1):
+                data_frame = pd.concat(data_frame_list, axis = 1)
+                data_frame_dict[key] = data_frame
+                data_frame_list = []
+                data_frame = pd.DataFrame()
+    return data_frame_dict
 
-
-get_data(exch_symbs, Interval.in_daily, 10)
-#tdf_1 = tdf.get_hist(symbol = "ADS", exchange = "XETR", interval = Interval.in_daily, n_bars = 5000)
-##print(tdf_1.head())
-##print(tdf_1.index)
-#tdf_1_close = tdf_1_close[::-1]
-#tdf_1_close.rename(columns={'close' : 'ADS'}, inplace = True)
-#print(tdf_1_close.head())
-#print(list(tdf_1_close.columns))
-#tdf_2 = tdf.get_hist(symbol = "AIR", exchange = "XETR", interval = Interval.in_daily, n_bars = 5000)
-#tdf_2_close = tdf_2[['close']]
-#tdf_2_close = tdf_2_close[::-1]
-#tdf_2_close.rename(columns = {'close' : 'AIR'}, inplace = True)
-#print(list(tdf_2_close.columns))#
-
-#a = pd.concat([tdf_1_close, tdf_2_close], axis = 1)
-
-#print(a.head())
